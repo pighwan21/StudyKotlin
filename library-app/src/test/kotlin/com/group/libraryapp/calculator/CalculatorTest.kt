8b3b2ef6ee68d1be31bf01313aaf1c616062d6d9
pighwan21 <pighwan21@gmail.com>
2023-07-03 15:19:34 +0900

    5강. 사칙연산 계산기의 나눗셈 테스트 작성

--- library-app/src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ library-app/src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,11 +1,14 @@
 package com.group.libraryapp.calculator
 
+import java.lang.Exception
+
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
     calculatorTest.minusTest()
     calculatorTest.multiplyTest()
     calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 class CalculatorTest {
 
@@ -56,18 +59,49 @@ class CalculatorTest {
         }
     }
 
+//    fun divideTest() {
+//        // given
+//        val calculator = Calculator(15)
+//
+//        // when
+//        calculator.divide(5)
+//
+//        // then
+//        if (calculator.number != 3) {
+//            throw IllegalArgumentException()
+//        }
+//    }
+
+    // 미리 나눗셈 테스트도 맹글어놨는데 다음 시간에 나눗셈 테스트 맹근다고 예고함..
+
     fun divideTest() {
         // given
-        val calculator = Calculator(15)
+        val calculator = Calculator(5)
 
         // when
-        calculator.divide(5)
+        calculator.divide(2)
 
         // then
-        if (calculator.number != 3) {
+        if (calculator.number != 2) {
             throw IllegalArgumentException()
         }
     }
 
-    // 미리 나눗셈 테스트도 맹글어놨는데 다음 시간에 나눗셈 테스트 맹근다고 예고함..
+    fun divideExceptionTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            if (e.message != "0으로 나눌 수 없습니다.")
+                throw IllegalStateException("메세지가 다릅니다.")
+            // 테스트 성공!!
+            return
+        } catch (e: Exception) {
+            throw IllegalStateException()
+        }
+        throw IllegalStateException("기대하는 예외가 발생하지 않았습니다.")
+    }
 }
