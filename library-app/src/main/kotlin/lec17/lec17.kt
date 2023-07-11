0c8c08ca5e54cd14928c1389c6f671f3d040ff66
pighwan21 <pighwan21@gmail.com>
2023-07-11 18:02:28 +0900

    .kt 파일 수정

--- library-app/src/main/kotlin/lec17/lec17.kt
+++ library-app/src/main/kotlin/lec17/lec17.kt
@@ -1,6 +1,8 @@
 package lec17
 
-// 18강. DTO를 Kotlin으로 변경하기
-// - DTO의 경우 간단하지만, 양이 많다는 것이 특징.
+// 17강
+// optional을 제거하고 엘비스 연산자를 사용
+// util 폴더에 ExceptionUtils파일을 맹글어 그곳에 fail 함수를 생성.
+// 반복되는 코드를 fail함수를 이용해서 리펙토링
 
-
+// findById의 경우 findByIdOrNull 확장함수를 이용해서 제어할 수 있음(optional을 뺄 수 잇음)
