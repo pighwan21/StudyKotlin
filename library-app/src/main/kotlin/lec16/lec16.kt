a0cf7ce44fae48c60e7d8f10bcc62995ea766c27
pighwan21 <pighwan21@gmail.com>
2023-07-11 17:28:33 +0900

    16강. 서비스 계층을 Kotlin으로 변경하기 - UserService.java

--- library-app/src/main/kotlin/lec16/lec16.kt
+++ library-app/src/main/kotlin/lec16/lec16.kt
@@ -1,3 +1,9 @@
 package lec16
 
 // 16강. 서비스 계층을 Kotlin으로 변경하기 - UserService.java
+
+// optional을 제거하고 엘비스 연산자를 사용
+// util 폴더에 ExceptionUtils파일을 맹글어 그곳에 fail 함수를 생성.
+// 반복되는 코드를 fail함수를 이용해서 리펙토링
+
+// findById의 경우 findByIdOrNull 확장함수를 이용해서 제어할 수 있음(optional을 뺄 수 잇음)
