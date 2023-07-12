8b9b9341259b7eec94d9ee94a9ca0c8cf9cf5e59
pighwan21 <pighwan21@gmail.com>
2023-07-12 15:53:05 +0900

    24강. 첫 번째 요구사항 클리어!

---
+++ library-app/src/main/kotlin/lec/lec24.kt
@@ -0,0 +1,7 @@
+package lec
+
+// 24강. 첫 번째 요구사항 클리어!
+// - 지난 시간까지 새로운 요구사항을 Enum 클래스를 활용해 구현했고, 기존에 있던 Boolean 필드 역시 Enum 클래스로 리팩토링했음.
+// 1. Type, Status 등을 서버에서 관리하는 방법, Boolean과 Enum의 장단점
+// 2. Test Fixture의 필요성과 구성 방법
+// 3. Kotlin에서 Enum + JPA + Spring Boot를 활용할 수 있는 방법
